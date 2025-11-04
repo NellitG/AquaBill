daea35563cec7cb793c487687c4bdf81e923d1f6
NellitG <123721407+NellitG@users.noreply.github.com>
2025-11-04 11:51:31 +0300

    Initial commit

---
+++ app/src/views/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <LoginEmail />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import LoginEmail from "../components/LoginEmail.vue";
+</script>
---
+++ app/src/views/index.js
@@ -0,0 +1,12 @@
+import HomeView from "./HomeView.vue";
+import DetailsView from "./DetailsView.vue";
+import LoginView from "./LoginView.vue";
+import PostsView from "./PostsView.vue";
+import PiniaTest from "./PiniaTest.vue";
+import TravelPlanView from "./TravelPlanView.vue";
+import InspirationView from "./InspirationView.vue";
+import ShopView from "./ShopView.vue";
+import SignUpView from "./SignUpView.vue";
+import DestinationView from "./DestinationView.vue";
+
+export { HomeView, DetailsView, LoginView, PostsView, PiniaTest, TravelPlanView, InspirationView, ShopView, SignUpView, DestinationView };
