5a99ec0dd5d62da28c21ee52678b60cd242ecf49
NellitG <kamaunelson157@gmail.com>
2025-11-06 15:37:54 +0300

    modify paths from clients, history, readings

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { HomeView, PostsView, DetailsView, LoginView, TravelPlanView, InspirationView, ShopView, SignUpView, DestinationView } from "../views";
+import { HomeView, PostsView, DetailsView, LoginView, ClientsView, ReadingsView, HistoryView, SignUpView, DestinationView } from "../views";
 import { useAuthStore } from "../store";
+import History from '@/components/History.vue';
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
   { path: "/login", name: "login", component: LoginView },
   { path: "/posts", name: "posts", component: PostsView },
   { path: "/posts/:id", name: "details", component: DetailsView, props: true },
-  {path:"/travelplan", name:"Travelplan", component: () => import("../views/TravelPlanView.vue")},
-  {path:"/inspiration", name:"Inspiration", component: () => import("../views/InspirationView.vue")},
-  {path:"/shop", name:"Shop", component: () => import("../views/ShopView.vue")},
+  {path:"/clients", name:"Clients", component: () => import("../views/ClientsView.vue")},
+  {path:"/readings", name:"Readings", component: () => import("../views/ReadingsView.vue")},
+  {path:"/history", name:"History", component: () => import("../views/HistoryView.vue")},
   {path: "/SignUp", name: "signup", component: () => import("../views/SignUpView.vue")},
   {path: "/destination", name: "destination", component: () => import("../views/DestinationView.vue")},
+  {path: "/history", component: History},
 ];
 
 /**Initialize here */
--- app/src/views/index.js
+++ app/src/views/index.js
@@ -3,10 +3,10 @@ import DetailsView from "./DetailsView.vue";
 import LoginView from "./LoginView.vue";
 import PostsView from "./PostsView.vue";
 import PiniaTest from "./PiniaTest.vue";
-import TravelPlanView from "./TravelPlanView.vue";
-import InspirationView from "./InspirationView.vue";
-import ShopView from "./ShopView.vue";
+import ClientsView from "./ClientsView.vue";
+import ReadingsView from "./ReadingsView.vue";
+import HistoryView from "./HistoryView.vue";
 import SignUpView from "./SignUpView.vue";
 import DestinationView from "./DestinationView.vue";
 
-export { HomeView, DetailsView, LoginView, PostsView, PiniaTest, TravelPlanView, InspirationView, ShopView, SignUpView, DestinationView };
+export { HomeView, DetailsView, LoginView, PostsView, PiniaTest, ClientsView, ReadingsView, HistoryView, SignUpView, DestinationView };
