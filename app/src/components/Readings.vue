9e09e9b9122ef4aac32ae12f23e215283dfaa954
NellitG <kamaunelson157@gmail.com>
2025-11-11 12:20:41 +0300

    correct url

--- app/src/components/Readings.vue
+++ app/src/components/Readings.vue
@@ -186,7 +186,7 @@ async function calculateBill() {
   try {
     const baseURL = import.meta.env.VITE_API_URL
     const client = clients.value.find(c => c.id === selectedClientId.value);
-    const response = await axios.post(`${baseURL}/api/clients/${selectedClientId.value}/calculate_bill/`,
+    const response = await axios.post(`${baseURL}/api/${selectedClientId.value}/calculate-bill/`,
       {
         current_reading: currentReading.value,
         rate_per_unit: ratePerUnit.value,
